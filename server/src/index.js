aa8346d3aebe8edd40ec8e7794ee16976b34df70
Conrad Perez <conrad_perez@yahoo.com>
2020-04-18 17:47:33 +0800

    02_databaseSetup

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
@@ -7,6 +8,14 @@ require('dotenv').config();
 
 // app
 const app = express();
+
+// db
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser    : true,
+  useCreateIndex     : true,
+  useUnifiedTopology : true,
+  useFindAndModify   : false
+}).then(() => console.log('DB connected'));
 
 // middleware
 app.use(morgan('dev'));
