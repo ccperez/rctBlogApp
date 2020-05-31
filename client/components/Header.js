9747f15eb90347738abf53deed5bfade0ccd6b80
Conrad Perez <conrad_perez@yahoo.com>
2020-05-31 20:20:32 +0800

    15_NavLink

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,45 +1,52 @@
 import { useState } from 'react';
+import Link from 'next/link';
 import { APPNAME } from '../config';
 
 import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	UncontrolledDropdown,
+	DropdownToggle,
+	DropdownMenu,
+	DropdownItem
 } from 'reactstrap';
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
+	const toggle = () => {
+		setIsOpen(!isOpen);
+	};
 
-  return (
-    <div>
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">{ APPNAME }</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    </div>
-  );
+	return (
+		<div>
+			<Navbar color="light" light expand="md">
+				<Link href="/">
+					<NavLink className="font-weight-bold">{APPNAME}</NavLink>
+				</Link>
+				<NavbarToggler onClick={toggle} />
+				<Collapse isOpen={isOpen} navbar>
+					<Nav className="ml-auto" navbar>
+						<NavItem>
+							<Link href="/signin">
+								<NavLink>Signin</NavLink>
+							</Link>
+						</NavItem>
+						<NavItem>
+							<Link href="/signup">
+								<NavLink>Signup</NavLink>
+							</Link>
+						</NavItem>
+					</Nav>
+				</Collapse>
+			</Navbar>
+		</div>
+	);
 };
 
 export default Header;
