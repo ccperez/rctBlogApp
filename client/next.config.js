71d67aaf8588fdc2c13f55689693e71aca87046c
Conrad Perez <conrad_perez@yahoo.com>
2020-06-19 17:45:25 +0800

    20_zeitNextCSS

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,10 @@
-module.exports = {
+const withCSS = require('@zeit/next-css');
+
+module.exports = withCSS({
 	publicRuntimeConfig: {
 		APP_NAME				: 'SEOBLOG',
 		API_DEVELOPMENT	: 'http://0.0.0.0:5000/api',
 		API_PRODUCTION	: 'http://0.0.0.0:5000/api',
 		PRODUCTION			: false
 	}
-}
+});
